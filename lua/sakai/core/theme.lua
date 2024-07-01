09b4b91148978437475bb5e6d6a29198183a0736
2ELCN0168 <lcn.en.perso@protonmail.com>
2024-07-01 17:21:55 +0200

    Added/modified files

--- lua/sakai/core/theme.lua
+++ lua/sakai/core/theme.lua
@@ -1,8 +1,8 @@
 -- Themery block
 -- This block will be replaced by Themery.
-vim.opt.background = "dark"
+vim.opt.background = "light"
 
 
-vim.cmd("colorscheme tokyonight-storm")
-vim.g.theme_id = 4
+vim.cmd("colorscheme tokyonight-day")
+vim.g.theme_id = 3
 -- end themery block
--- lua/sakai/plugins/conform.lua
+++ lua/sakai/plugins/conform.lua
@@ -4,41 +4,51 @@
 --]]
 
 return {
-	"stevearc/conform.nvim",
-	event = { "BufReadPre", "BufNewFile" },
-	config = function()
-		local conform = require("conform")
+        'stevearc/conform.nvim',
+        event = { 'BufReadPre', 'BufNewFile' },
+        config = function()
+                require('conform').setup {
+                        formatters_by_ft = {
+                                javascript = { 'prettier' },
+                                typescript = { 'prettier' },
+                                javascriptreact = { 'prettier' },
+                                typescriptreact = { 'prettier' },
+                                svelte = { 'prettier' },
+                                css = { 'prettier' },
+                                html = { 'prettier' },
+                                json = { 'prettier' },
+                                yaml = { 'prettier' },
+                                markdown = { 'prettier' },
+                                graphql = { 'prettier' },
+                                liquid = { 'prettier' },
+                                lua = { 'stylua' },
+                                python = { 'isort', 'black' },
+                                c = { 'clang-format' },
+                        },
+                        format_on_save = {
+                                lsp_fallback = true,
+                                async = false,
+                                timeout_ms = 1000,
+                        },
+                        formatters = {
+                                ['clang-format'] = {
+                                        args = {
+                                                --'--style={BasedOnStyle: LLVM, IndentWidth: 8, TabWidth: 8, UseTab: Never, AlignAfterOpenBracket: Align, AlignArrayOfStructures: Right, AlignTrailingComments: Always, ColumnLimit: 80, PointerAlignment: Right, ReferenceAlignment: Right, SpaceBeforeAssignmentOperators: true, SpaceBeforeCaseColon: false, SpaceBeforeSquareBrackets: false, SpaceInEmptyBlock: false, SpacesInParentheses: false, SpacesInSquareBrackets: false, AllowShortFunctionsOnASingleLine: Empty}',
+                                                '--style={BasedOnStyle: LLVM, IndentWidth: 8, TabWidth: 8, UseTab: Always, BreakBeforeBraces: Linux, AllowShortIfStatementsOnASingleLine: false, IndentCaseLabels: false, ColumnLimit: 80, PointerAlignment: Right, AllowShortBlocksOnASingleLine: Empty, AllowShortFunctionsOnASingleLine: Empty}',
+                                        },
+                                        stdin = true,
+                                },
+                        },
+                }
 
-		conform.setup({
-			formatters_by_ft = {
-				javascript = { "prettier" },
-				typescript = { "prettier" },
-				javascriptreact = { "prettier" },
-				typescriptreact = { "prettier" },
-				svelte = { "prettier" },
-				css = { "prettier" },
-				html = { "prettier" },
-				json = { "prettier" },
-				yaml = { "prettier" },
-				markdown = { "prettier" },
-				graphql = { "prettier" },
-				liquid = { "prettier" },
-				lua = { "stylua" },
-				python = { "isort", "black" },
-			},
-			format_on_save = {
-				lsp_fallback = true,
-				async = false,
-				timeout_ms = 1000,
-			},
-		})
-
-		vim.keymap.set({ "n", "v" }, "<leader>cp", function()
-			conform.format({
-				lsp_fallback = true,
-				async = false,
-				timeout_ms = 1000,
-			})
-		end, { desc = "Format file or range (in visual mode)" })
-	end,
+                vim.keymap.set({ 'n', 'v' }, '<leader>cp', function()
+                        require('conform').format {
+                                lsp_fallback = true,
+                                async = false,
+                                timeout_ms = 1000,
+                        }
+                end, {
+                        desc = 'Format file or range (in visual mode)',
+                })
+        end,
 }
