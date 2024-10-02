280427e5fcae7f73765fe5475ed6128304dcb093
2ELCN0168 <lcn.en.perso@protonmail.com>
2024-10-02 19:44:39 +0200

    Added personnal keywords

--- lua/sakai/plugins/todo-comments.lua
+++ lua/sakai/plugins/todo-comments.lua
@@ -4,23 +4,53 @@
 --]]
 
 return {
-	"folke/todo-comments.nvim",
-	event = { "BufReadPre", "BufNewFile" },
-	dependencies = { "nvim-lua/plenary.nvim" },
+        'folke/todo-comments.nvim',
+        event = { 'BufReadPre', 'BufNewFile' },
+        dependencies = { 'nvim-lua/plenary.nvim' },
 
-	config = function()
-		local todo_comments = require("todo-comments")
+        config = function()
+                local todo_comments = require 'todo-comments'
 
-		local keymap = vim.keymap
+                local keymap = vim.keymap
 
-		keymap.set("n", "]t", function()
-			todo_comments.jump_next()
-		end, { desc = "Next todo comment" })
+                keymap.set('n', ']t', function()
+                        todo_comments.jump_next()
+                end, { desc = 'Next todo comment' })
 
-		keymap.set("n", "[t", function()
-			todo_comments.jump_prev()
-		end, { desc = "Previous todo comment" })
+                keymap.set('n', '[t', function()
+                        todo_comments.jump_prev()
+                end, { desc = 'Previous todo comment' })
 
-		todo_comments.setup()
-	end,
+                todo_comments.setup {
+                        keywords = {
+                                COMMAND = {
+                                        icon = '  ',
+                                        color = 'info',
+                                        alt = {
+                                                'CODE',
+                                                'SNIPPET',
+                                                'SCRIPT',
+                                                'ALGORITHM',
+                                                'ALGO',
+                                        },
+                                },
+
+                                COLOR = {
+                                        icon = '  ',
+                                        color = 'default',
+                                        alt = {
+                                                'COLOUR',
+                                                'HEX',
+                                                'RGB',
+                                                'RVB',
+                                                'RGBA',
+                                                'RVBA',
+                                                'HSL',
+                                                'TSL',
+                                                'TONE',
+                                        },
+                                },
+                        },
+                }
+        end,
 }
